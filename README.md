ea29ff82f29dbb32b4c1fc691c2982b6e2bf3128
Julian Sovernigo <juliansovernigo@gmail.com>
2023-10-01 21:54:22 -0400

    Modified to allow the use of API tokens instead of the global API key for a cloudflare account.

--- README.md
+++ README.md
@@ -17,8 +17,8 @@ Everything is Makefiled for you. Follow these steps for an easy setup:
 1. run `make .env``, fill it out with the correct data.
 2. navigate to dash.cloudflare.com, log in.
     - collect your account ID from the lefthand panel under the API section.
-    - collect your global key as well - this gets moved around a lot, but it is usually hard to find.
+    - create and collect an API token under the api token section. record this; it won't be shown again.
 3. create a file called `accountid`, put your account id from step 2.1 into it.
-4. create a file called `globalkey`, put your global key from step 2.2 into it.
+4. create a file called `token`, put your api token from step 2.2 into it.
 5. run `make build`
 6. run `make run`
--- cfddns.sh
+++ cfddns.sh
@@ -3,8 +3,8 @@
 myip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 path=$(dirname $(realpath $0))
 
-globalkey=$(cat /run/secrets/globalkey)
 accountid=$(cat /run/secrets/accountid)
+token=$(cat /run/secrets/token)
 
 function hardquit () {
     exit 0
@@ -13,8 +13,7 @@ trap 'hardquit' SIGINT
 
 function getzone () {
     echo $(curl -X GET "${APIBASE}/zones?name=${DOMAIN}" \
-        -H "X-Auth-Email: $EMAIL" \
-        -H "X-Auth-Key: $globalkey" \
+        -H "Authorization: Bearer $token" \
         -H "Content-Type: application/json" \
         | jq -r ".result[$1].id" \
         | sed s/\"//g)
@@ -22,8 +21,7 @@ function getzone () {
 
 function getdns () {
     echo $(curl -X GET "${APIBASE}/zones/$1/dns_records" \
-        -H "X-Auth-Email: $EMAIL" \
-        -H "X-Auth-Key: $globalkey" \
+        -H "Authorization: Bearer $token" \
         -H "Content-Type: application/json" \
         | jq ".result[]|select(.name==\"${SUBDOMAIN}.${DOMAIN}\")|.id" \
         | sed s/\"//g)
@@ -41,8 +39,7 @@ TTL="600";
 while :
 do 
     curl -X PUT "${APIBASE}/zones/$zone/dns_records/$dns_id" \
-        -H "X-Auth-email: $EMAIL" \
-        -H "X-Auth-Key: $globalkey" \
+        -H "Authorization: Bearer $token" \
         -H "Content-Type: application/json" \
         --data '{"type":"'"$TYPE"'","name":"'"$NAME"'","content":"'"$CONTENT"'","proxied":'"$PROXIED"',"ttl":'"$TTL"'}'
     echo "last ran $(date)." >> $path/run.log
